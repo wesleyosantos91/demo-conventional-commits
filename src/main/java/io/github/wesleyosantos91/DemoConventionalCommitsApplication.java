342acd0f3fe86932a11c16d9ff83d2eda7622555
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2024-02-22 20:19:50 -0300

    first commit

---
+++ src/main/java/io/github/wesleyosantos91/DemoConventionalCommitsApplication.java
@@ -0,0 +1,13 @@
+package io.github.wesleyosantos91;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConventionalCommitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConventionalCommitsApplication.class, args);
+	}
+
+}
